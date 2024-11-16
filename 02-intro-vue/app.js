69d743a9f853ca3712cda6751ffbd9c1e80c84e7
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2024-11-16 00:27:05 -0500

    Add hello world vue

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -5,10 +5,25 @@ const {createApp, ref} = Vue;
 const app = createApp(
     {
     template: `
-    <h1>Hello World </h1>
-    <p>From app.js</p>
+    <h1>{{message}}</h1>
+    <p>{{author}}</p>
+    `,
+    setup() {
 
-    `
+        const message = ref("I'm Cris");
+        const author = ref('Bruce Wayne');
+        setTimeout(() => {
+
+            message.value = 'Cris Sánchez BTW';
+            author.value = 'My nick name is actually Crisi';
+        }, 1000);
+
+
+        return {message, author};
+
+    }
 });
+
+
 
 app.mount('#myApp');
