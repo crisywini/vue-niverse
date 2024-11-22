2509b83ae4486fbc93acd24254ae8455b77755ae
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2024-11-22 16:18:55 -0500

    Add app vue component

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,10 +1,30 @@
 <template>
     <h1>Hello World</h1>
+    <section>
+
+        <h3>Counter: {{ counter }}</h3>
+        <h3>Square: {{ squareCounter }}</h3>
+        <div>
+            <button @click="increment">+1</button>
+            <button @click="counter--">-1</button>
+        </div>
+    </section>
 </template>
 
 <script lang="ts" setup>
+    import {computed, ref} from 'vue';
 
-    console.log('Hello World from Perficient office')
+    const counter = ref(0);
+    const squareCounter = computed(() => counter.value * counter.value);
+
+    const increment = () => {
+        counter.value++;
+    }
+
+    const decrement = () => {
+        counter.value--;
+    }
+
 </script>
 
 
